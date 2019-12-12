5bc3df55e3f8c3b00de8d5fac3368cae62c2c9d6
Jen Rogers <jen@sci.utah.edu>
2019-12-11 18:47:41 -0700

    adding new stuff

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,9 +2,15 @@ import '../styles/index.scss';
 import * as d3 from 'd3';
 import { addCitations } from './citationLoader';
 
+/**
+ * This is the background Section
+ */
 
 
 
+/**
+ * This is the References Section
+ */
 let wrapDomain = d3.select('#main').append('div').classed('citation-wrap', true);
 let wrapProcess = d3.select('#main').append('div').classed('citation-wrap', true);
 let wrapRelated = d3.select('#main').append('div').classed('citation-wrap', true);
